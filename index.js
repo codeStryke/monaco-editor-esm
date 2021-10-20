745fb77581b7c6f705bc69239bd190d7ad756ec2
Anand Raman <araman@netfix.com>
2021-10-20 16:59:22 -0700

    added a comment about inlining the web worker

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import * as monaco from 'monaco-editor'
-import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker&inline'
+import * as monaco from 'monaco-editor';
+// Inlining the import of web worker since the path from where it will be served is not known at compile time
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker&inline';
 
 self.MonacoEnvironment = {
   getWorker() {
